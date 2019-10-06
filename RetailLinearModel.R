46ae27577acd678125621a72d3e768e921c77b21
wcococow <wcococow@gmail.com>
2019-10-06 22:24:32 +0800

    20019100602

--- RetailLinearModel.R
+++ RetailLinearModel.R
@@ -1,11 +1,30 @@
 library(dplyr)
 library(tidyverse)
-
+CreateDummies=function(data,var,freq_cutoff=0){
+  t=table(data[,var])
+  t=t[t>freq_cutoff]
+  t=sort(t)
+  categories=names(t)[-1]
+  
+  for( cat in categories){
+    name=paste(var,cat,sep="_")
+    name=gsub(" ","",name)
+    name=gsub("-","_",name)
+    name=gsub("\\?","Q",name)
+    name=gsub("<","LT_",name)
+    name=gsub("\\+","",name)
+    
+    data[,name]=as.numeric(data[,var]==cat)
+  }
+  
+  data[,var]=NULL
+  return(data)
+}
 
 setwd("D:/edvancer/R project/Retail")
 
-data.test <- read.csv("store_test.csv")
-data.train <- read.csv("store_train.csv")
+data.test <- read.csv("store_test.csv", stringsAsFactors = F)
+data.train <- read.csv("store_train.csv", stringsAsFactors = F)
 
 data.test$store <- NA
 data.train$data  <- 'train'
@@ -15,7 +34,13 @@ data <- rbind(data.train,data.test)
 data$country[which(is.na(data$country))] <-5
 
 summary(data)
-
-x <-data %>% filter(is.na(population))
+str(data)
 data$population[which(data$Areaname=="Franklin County, ME" & data$countytownname == "Madrid town")] = 173
 data$population[which(data$Areaname=="Washington County, ME" & data$countytownname == "Centerville town")] = 26
+#countyname storecode Areaname countytownname state_alpha store_Type
+data <- data %>% select(-countyname, -storecode, -Areaname, -countytownname)
+data <- CreateDummies(data ,"store_Type",3)
+data <- CreateDummies(data ,"state_alpha",53)
+
+data.train=data %>% filter(data=='train') %>% select(-data)
+data.test=data %>% filter(data=='test') %>% select(-data,-store)
